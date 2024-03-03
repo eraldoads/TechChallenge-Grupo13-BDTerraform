ec6b9c9a85ffbf98ba4d72c9b140373f6519fa1b
Luís Felipe Amengual Tatsch <lfelipe_at@yahoo.com.br>
2024-03-03 15:00:46 -0300

    Retirada de comentário

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "piklesfastfood"
     key    = "BDTerraform/terraform.tfstate"
-    region = "us-east-1" # Escolha sua região
+    region = "us-east-1"
   }
 }
 
